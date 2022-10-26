232bec6f7a2940929a043c4df4aa912012c8d7bf
Dan <melnikov.danila2013@gmail.com>
2022-10-26 16:48:52 +0300

    After brain-even is ready

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DanilaMiller/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/DanilaMiller/python-project-49/actions)
+
+https://asciinema.org/a/4BePS2zvyAq1aLk69OCCRGQ5n
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,41 @@
+import random
+import prompt
+
+
+def main():
+    print(f'Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+
+    while i < 3:
+      number = random.randint(1,20)
+
+      print(f'Question: {number}')
+      result = prompt.string('Your answer: ')
+
+      if number % 2 == 0 and result == "yes":
+        print("Correct!")
+      elif number % 2 != 0 and result == "no":
+        print("Correct!")
+      elif number % 2 == 0 and result == "no":
+        print("'no' is wrong answer ;(. Correct answer was 'yes'.")
+        print(f"Let's try again, {name}!")
+        break
+      elif number % 2 != 0 and result == "yes":
+        print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+        print(f"Let's try again, {name}!")
+        break
+      else:
+        print(f"Wrong input!")
+        print(f"Let's try again, {name}!")
+        break
+
+      i += 1
+      if i == 3:
+        print(f'Congratulations {name}!')
+
+
+if __name__ == '__main__':
+    main()
